d5113c474738d46092643cd83001537d6fc1a165
Axel <axel.louis.1991@gmail.com>
2020-07-27 05:52:49 +0200

    able to inflate non ms bzip

--- program/Program.cs
+++ program/Program.cs
@@ -1,13 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using DecoderLib;
 
 namespace Program
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var decoder = new DecoderLib.Decoder();
-            Console.WriteLine("Hello World!");
+
+            foreach(string arg in args)  
+            {
+                using (FileStream input = File.OpenRead(arg))
+                {
+                    input.Seek(2, SeekOrigin.Begin);
+
+                    string newFileName = $"{arg}.json";
+
+                    using (FileStream output = File.Create(newFileName))
+                    {
+                        using (DeflateStream  bzis = new DeflateStream (input, CompressionMode.Decompress))
+                        {
+                            bzis.CopyTo(output);
+                            Console.WriteLine("Decompressed: {0}", arg);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
